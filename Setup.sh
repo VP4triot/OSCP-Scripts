0b44e92f6e7e406f8a9a75d67fc500e705e6ba3c
Vp4triot <51681458+VP4triot@users.noreply.github.com>
2023-09-23 15:21:24 -0300

    Create Setup.sh
    This is the setup script if my VM breaks and I need to setup a new one that has the tools and things I like ready

---
+++ Setup.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/danielmiessler/SecLists /opt/SecLists &
+apt-get install libreoffice &
+wait
+echo 'The tasks have been completed! System is ready to hack!'
+                                                                                                                    
