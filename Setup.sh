dbc22d2d46623d7318c2e0816a23c9777d4ec3f4
Vp4triot <51681458+VP4triot@users.noreply.github.com>
2023-09-23 15:44:58 -0300

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,5 +1,8 @@
 git clone https://github.com/danielmiessler/SecLists /opt/SecLists &
 apt-get install libreoffice &
 wait
+echo 'Installing pip2 for python2 because I wanna save time'
+wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
+python2 get-pip.py
 echo 'The tasks have been completed! System is ready to hack!'
                                                                                                                     
