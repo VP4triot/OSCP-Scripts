811b552866146c9cd752ce99020df2cdc31ed4ee
Vp4triot <51681458+VP4triot@users.noreply.github.com>
2023-11-10 20:10:15 -0300

    Update Powershell_GetString.ps1

--- Powershell_GetString.ps1
+++ Powershell_GetString.ps1
@@ -4,6 +4,9 @@ $rootDirectory = "C:\Path\To\Your\Directory"
 # Define the string to search for
 $searchString = "YourSearchString"
 
+# Specify the output file
+$outputFile = "C:\Path\To\Your\Output\File.txt"
+
 # Use Get-ChildItem to get all files in the directory and its subdirectories
 $files = Get-ChildItem -Path $rootDirectory -Recurse | Where-Object { $_.PSIsContainer -eq $false }
 
@@ -11,10 +14,10 @@ $files = Get-ChildItem -Path $rootDirectory -Recurse | Where-Object { $_.PSIsCon
 foreach ($file in $files) {
     $result = Get-Content $file.FullName | Select-String -Pattern $searchString -Context 2
     foreach ($match in $result) {
-        Write-Host "String found in file: $($file.FullName)"
-        Write-Host "  Match: $($match.Line)"
-        Write-Host "  Context Before: $($match.Context.PreContext -join "`n")"
-        Write-Host "  Context After: $($match.Context.PostContext -join "`n")"
-        Write-Host ""
+        "$($file.FullName)" | Out-File -Append -FilePath $outputFile
+        "  Match: $($match.Line)" | Out-File -Append -FilePath $outputFile
+        "  Context Before: $($match.Context.PreContext -join "`n")" | Out-File -Append -FilePath $outputFile
+        "  Context After: $($match.Context.PostContext -join "`n")" | Out-File -Append -FilePath $outputFile
+        "" | Out-File -Append -FilePath $outputFile
     }
 }
